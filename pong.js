6abcb9ca8915cf3de3de9c13381b79ad6156aeed
João Marcos Aires Duarte <joao.duarte@ITBAM.ORG.BR>
2022-01-05 14:21:56 -0400

    Possibilitando o erro da raquete do oponente

--- pong.js
+++ pong.js
@@ -1,7 +1,7 @@
 //variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
-let diametro = 20;
+let diametro = 13;
 let raio = diametro / 2 ;
 
 //velocidade da bolinha
@@ -14,26 +14,27 @@ let raqueteAltura = 90;
 let xRaquete = 5;
 let yRaquete = 150;
 
-//variáveis da raquete do oponente
+//variáveis do oponente
 let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
 let velocidadeYOponente;
+let chanceDeErrar = 0;
 
 let colidiu = false;
 
-//plcar do jogo
+//placar do jogo
 let meusPontos = 0;
 let pontosDoOponente = 0;
 
 //sons do jogo
-let raquetada;
 let ponto;
+let raquetada;
 let trilha;
 
 function preload(){
   trilha = loadSound("trilha.mp3");
-  ponto = loadSound("ponto.mp3");
   raquetada = loadSound("raquetada.mp3");
+  ponto = loadSound("ponto.mp3");
 }
 
 function setup() {
@@ -47,10 +48,11 @@ function draw() {
   movimentaBolinha();
   verificaColisaoBorda();
   mostraRaquete(xRaquete, yRaquete);
-  mostraRaquete(xRaqueteOponente, yRaqueteOponente)
   movimentaMinhaRaquete();
-  //movimentaRaqueteOponente();
+  //verificaColisaoRaquete();
   verificaColisaoRaquete(xRaquete, yRaquete);
+  mostraRaquete(xRaqueteOponente, yRaqueteOponente);
+  movimentaRaqueteOponente();
   verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
   incluiPlacar();
   marcaPonto();
@@ -76,11 +78,10 @@ function verificaColisaoBorda(){
   }
 }
 
-function mostraRaquete(x, y){
+function mostraRaquete(x,y){
   rect(x, y, raqueteComprimento, 
       raqueteAltura);
 }
-
 
 function movimentaMinhaRaquete(){
   if (keyIsDown(UP_ARROW)){
@@ -92,50 +93,61 @@ function movimentaMinhaRaquete(){
 }
 
 function verificaColisaoRaquete(){
-  if (xBolinha - raio < xRaquete + raqueteComprimento && 
-      yBolinha - raio < yRaquete + raqueteAltura && 
-      yBolinha + raio > yRaquete){
+  if (xBolinha - raio < xRaquete + raqueteComprimento && yBolinha - raio < yRaquete + raqueteAltura && yBolinha + raio > yRaquete){
+    velocidadeXBolinha *= -1;
+  }
+}
+
+function verificaColisaoRaquete(x, y){
+  colidiu = collideRectCircle(x, y,raqueteComprimento,raqueteAltura,
+xBolinha,yBolinha,raio);
+  if (colidiu){
     velocidadeXBolinha *= -1;
     raquetada.play();
   }
 }
-function verificaColisaoRaquete(x, y) {
-    colidiu = collideRectCircle(x, y, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
-    if (colidiu){
-        velocidadeXBolinha *= -1;
-      raquetada.play();
-    }
-}
 
-function movimentaRaqueteOponente() {
-  velocidadeYOponente = yBolinha - yRaqueteOponente - raqueteComprimento / 2 - 30;
-  yRaqueteOponente += velocidadeYOponente;
-  
+function movimentaRaqueteOponente(){
+  velocidadeYOponente = yBolinha -yRaqueteOponente - raqueteComprimento / 2 - 30;
+  yRaqueteOponente += velocidadeYOponente + chanceDeErrar
+  calculaChanceDeErrar()
 }
 
 function incluiPlacar(){
   stroke(255);
-  textAlign(CENTER)
-  textSize(16)
-  fill(color(255, 140, 0))
+  textAlign(CENTER);
+  textSize(16);
+  fill(color(255, 140, 0));
   rect(150, 10, 40, 20);
   fill(255);
-  text(meusPontos, 170, 26)
-  fill(color(255, 140, 0))
-  rect(405, 10, 40, 20);
+  text(meusPontos, 170, 26);
+  fill(color(255, 140, 0));
+  rect(450, 10, 40, 20);
   fill(255);
-  text(pontosDoOponente, 425, 26)
+  text(pontosDoOponente, 470, 26);
 }
 
 function marcaPonto(){
-  if(xBolinha > 590){
-     meusPontos += 1;
+  if (xBolinha > 590){
+    meusPontos += 1;
     ponto.play();
   }
-  if(xBolinha < 10){
+  if (xBolinha < 10){
     pontosDoOponente += 1;
     ponto.play();
   }
-  
-  
+}
+
+function calculaChanceDeErrar() {
+  if (pontosDoOponente >= meusPontos) {
+    chanceDeErrar += 1
+    if (chanceDeErrar >= 39){
+    chanceDeErrar = 40
+    }
+  } else {
+    chanceDeErrar -= 1
+    if (chanceDeErrar <= 35){
+    chanceDeErrar = 35
+    }
+  }
 }
